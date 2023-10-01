17809b5b3dcc0d57236a8701edfa3c57023f592c
evn1111 <138611344+evn1111@users.noreply.github.com>
2023-10-01 20:30:03 +0300

    LocalSettings.php

---
+++ LocalSettings.php
@@ -0,0 +1,12 @@
+<?php
+# База данных
+$wgDBtype = 'mysql';
+$wgDBserver = 'db';
+$wgDBname = 'mediawiki';
+$wgDBuser = 'wiki';
+$wgDBpassword = 'DEP@ssw0rd';
+
+# URL-адрес сайта
+$wgServer = 'http://localhost:8080';
+
+# Другие настройки...
