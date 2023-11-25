e084e9a94b96237f478d84be419fa5008a3ca47f
evn1111 <138611344+evn1111@users.noreply.github.com>
2023-11-25 15:14:40 +0300

    Update backup.bash

--- backup.bash
+++ backup.bash
@@ -1,9 +1,10 @@
-!/bin/bash
-backup_dir="/backup"
-cp -r /etc/ $backup_dir
-cp -r /home/ $backup_dir
-backup_file="backup_$(date +%Y%m%d%H%M%S).tar.gz"
-tar -czvf $backup_dir/$backup_file.tar.gz $backup_dir/etc/*
-tar -cvzf $backup_dir/$backup_file $backup_dir/home/*
-rm -rf $backup_dir/etc/
-rm -rf $backup_dir/home
+#!/bin/bash
+backup_files="/home /etc"
+dest="/opt/backup"
+day=$(date +%A-%F)
+hostname=$(hostname -s)
+archive_file="$hostname-$day.tgz"
+tar czf $dest/$archive_file $backup_files
+echo "Backup is done"
+date +%A-$F-%T
+ls -lh $dest
